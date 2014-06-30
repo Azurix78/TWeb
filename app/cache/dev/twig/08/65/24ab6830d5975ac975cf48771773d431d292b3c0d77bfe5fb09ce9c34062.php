2e2d0db53f1440b50190951a4ec6c8bbdbfec168
happy1337 <happy-666@hotmail.fr>
2014-06-30 22:28:44 +0200

    archi

---
+++ app/cache/dev/twig/08/65/24ab6830d5975ac975cf48771773d431d292b3c0d77bfe5fb09ce9c34062.php
@@ -0,0 +1,32 @@
+<?php
+
+/* RocketUsersBundle:Default:index.html.twig */
+class __TwigTemplate_086524ab6830d5975ac975cf48771773d431d292b3c0d77bfe5fb09ce9c34062 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello users!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "RocketUsersBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
